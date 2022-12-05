0dfef6a4a77d38b6500db8fea99aae4c288fd4b4
Hriddhi Mondal <hriddhi1990@gmail.com>
2022-12-05 17:19:03 +0530

    Completed implementing compress and save

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
 import re
+import os
+from PIL import Image
 
 
-def parse_command():
+def parse_command(sys_argv):
     '''
-    Function to parse commands
+    Function to parse arguments from the command line
+
+    Returns a dict of arguments
     '''
 
-    print("cmd entry: ", cmds := sys.argv)
+    print("cmd entry: ", cmds := sys_argv)
 
     params = {
         '-q': 80
@@ -32,5 +36,39 @@ def parse_command():
     return params
 
 
+def open_image_file(path):
+    if os.path.exists(path) and os.path.isfile(path):
+        return Image.open(path)
+    else:
+        raise FileNotFoundError(f'open_image_file(): file not found: {path}')
+
+
+def check_output_path(path):
+    if os.path.isfile(path):
+        overwrite_choice = input(
+            'file already exists at output. Overwrite file? (Y/n) ')
+        if overwrite_choice.lower() == 'y':
+            return True
+        elif overwrite_choice.lower() == 'n':
+            raise FileExistsError(
+                f'check_output_path(): file already exist at output: {path}')
+        else:
+            raise ValueError(
+                f'check_output_path(): invalid choice: {overwrite_choice}')
+
+    return True
+
+
+def compress_and_save_image(image, path, quality):
+    try:
+        image.save(path, quality=int(quality), optimize=True)
+    except FileNotFoundError as err:
+        os.mkdir(os.path.dirname(path))
+        image.save(path, quality=int(quality), optimize=True)
+
+
 if __name__ == '__main__':
-    parse_command()
+    params = parse_command(sys.argv)
+    image = open_image_file(params['-i'])
+    if check_output_path(params['-o']):
+        compress_and_save_image(image, params['-o'], params['-q'])
