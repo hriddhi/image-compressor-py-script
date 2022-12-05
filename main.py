7549c0ea6bde8f5d3f93d4eb59d388f9a8734e37
Hriddhi Mondal <hriddhi1990@gmail.com>
2022-12-05 15:54:00 +0530

    Implemented function to parse command params

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 import sys
-from PIL import Image
+import re
 
-print(sys.argv)
+
+def parse_command():
+    '''
+    Function to parse commands
+    '''
+
+    print("cmd entry: ", cmds := sys.argv)
+
+    params = {
+        '-q': 80
+    }
+
+    for i, cmd in enumerate(iterable=cmds, start=0):
+        if i == 0:
+            continue
+
+        regex = re.compile(r"^[-].+")
+        if regex.match(cmd):
+            if cmd == "-i" or cmd == "-o" or cmd == "-q":
+                params[cmd] = cmds[i+1]
+            else:
+                raise ValueError(f'parse_command(): invalid argument: {cmd}')
+
+    mandatory_params = ['-i', '-o']
+    for param in mandatory_params:
+        if param not in params.keys():
+            raise ValueError(f'parse_command(): argument not found: {param}')
+
+    return params
+
+
+if __name__ == '__main__':
+    parse_command()
